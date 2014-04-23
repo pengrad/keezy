8af1efdbdc506491abe0f0aabfbd9800a4c167c2
Stas Parshin <pengrad@bk.ru>
2014-04-24 01:03:44 +0400

    Simple rec-play logic

--- Keezy/src/main/java/com/pengrad/keezy/MainActivity.java
+++ Keezy/src/main/java/com/pengrad/keezy/MainActivity.java
@@ -5,16 +5,20 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import com.pengrad.keezy.logic.*;
+import com.pengrad.keezy.logic.MediaRecordManager;
+import com.pengrad.keezy.logic.PlayManager;
+import com.pengrad.keezy.logic.RecordManager;
+import com.pengrad.keezy.logic.SoundPoolPlayManager;
+import com.pengrad.keezy.ui.RecPlayButton;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.Touch;
 import org.androidannotations.annotations.ViewById;
 
 import java.io.IOException;
 
 import static com.pengrad.keezy.Utils.log;
+import static com.pengrad.keezy.Utils.toast;
 
 /**
  * User: stas
@@ -24,24 +28,72 @@ import static com.pengrad.keezy.Utils.log;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity implements View.OnTouchListener {
 
+    @ViewById
+    protected RecPlayButton button1, button2, button3, button4, button5, button6, button7, button8;
+
+    private RecPlayButton[] buttons;
     private RecordManager recordManager;
     private PlayManager playManager;
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        recordManager = new MediaRecordManager(2);
-        playManager = new SoundPoolPlayManager(2);
-        button1.setOnTouchListener(this);
-    }
-
-    @ViewById
-    Button button1, button2, button3, button4, button5, button6, button7, button8;
 
     public static final String FILE_1 = Environment.getExternalStorageDirectory() + "/myaudio.3gp";
     public static final String FILE_2 = Environment.getExternalStorageDirectory() + "/myaudio2.3gp";
 
-    @Touch
-    protected synchronized void button1Touched(View view, MotionEvent event) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        recordManager = new MediaRecordManager(2);
+        playManager = new SoundPoolPlayManager(2);
+    }
+
+    @AfterViews
+    protected void initViews() {
+        buttons = new RecPlayButton[]{button1, button2, button3, button4, button5, button6, button7, button8};
+        for (View button : buttons) {
+            button.setOnTouchListener(this);
+        }
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        if (!(view instanceof RecPlayButton)) return false;
+        RecPlayButton button = (RecPlayButton) view;
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                button.setPressed(true);
+                if (button.isRec()) {
+                    enableControls(button, false);
+                    rec(true);
+                } else {
+                    play(true);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_OUTSIDE:
+                button.setPressed(false);
+                if (button.isRec()) {
+                    rec(false);
+                    enableControls(button, true);
+                    button.makePlay();
+                }
+                break;
+        }
+        return true;
+    }
+
+    private void enableControls(View view, boolean enable) {
+        for (View button : buttons) {
+            if (button.getId() != view.getId()) button.setEnabled(enable);
+        }
+    }
+
+    protected void rec(boolean start) {
+        toast(this, start ? "start rec" : "stop rec");
+    }
+
+    protected void play(boolean start) {
+        toast(this, start ? "start play" : "stop play");
+    }
+
+    protected void button1Touched(View view, MotionEvent event) {
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
                 startRecording(1);
@@ -54,50 +106,6 @@ public class MainActivity extends Activity implements View.OnTouchListener {
                 break;
         }
     }
-
-    @Touch
-    protected synchronized void button2Touched(View view, MotionEvent event) {
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                startRecording(2);
-                view.setPressed(true);
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_OUTSIDE:
-                view.setPressed(false);
-                stopRecording(2);
-                break;
-        }
-    }
-
-    @Touch
-    protected synchronized void button3Touched(View view, MotionEvent event) {
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                startPlay(1);
-                view.setPressed(true);
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_OUTSIDE:
-                view.setPressed(false);
-                break;
-        }
-    }
-
-    @Touch
-    protected void button4Touched(View view, MotionEvent event) {
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                startPlay(2);
-                view.setPressed(true);
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_OUTSIDE:
-                view.setPressed(false);
-                break;
-        }
-    }
-
 
     @Background
     protected void startRecording(int i) {
@@ -119,7 +127,4 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     }
 
 
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        return true;
-    }
 }
