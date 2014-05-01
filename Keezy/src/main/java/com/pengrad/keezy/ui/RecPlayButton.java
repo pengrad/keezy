9e56506eaae9c9a66c6a9b6c043c9b76c164bc23
Stas Parshin <pengrad@bk.ru>
2014-05-01 22:27:48 +0400

    Show/hide recording icon on button

--- Keezy/src/main/java/com/pengrad/keezy/ui/RecPlayButton.java
+++ Keezy/src/main/java/com/pengrad/keezy/ui/RecPlayButton.java
@@ -1,9 +1,9 @@
 package com.pengrad.keezy.ui;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import com.pengrad.keezy.R;
 
@@ -31,18 +31,22 @@ public class RecPlayButton extends ImageView {
 
     private int padding;
     private boolean recording;
+    private Drawable imageRecord;
 
     private void init() {
-        padding = getResources().getDimensionPixelSize(R.dimen.button_press_padding);
         recording = true;
+        padding = getResources().getDimensionPixelSize(R.dimen.button_press_padding);
+        imageRecord = getResources().getDrawable(R.drawable.ic_recording);
     }
 
     public void makeRec() {
         recording = true;
+        setImageDrawable(imageRecord);
     }
 
     public void makePlay() {
         recording = false;
+        setImageDrawable(null);
     }
 
     public boolean isRec() {
