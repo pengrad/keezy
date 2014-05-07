532bc59c1bfa85779d1e76dc1db4849407af9056
Stas Parshin <pengrad@bk.ru>
2014-05-07 23:31:39 +0400

    New player based on MediaPlayer instead of SoundPool

---
+++ Keezy/src/main/java/com/pengrad/keezy/sound/MediaPlayerManager.java
@@ -0,0 +1,44 @@
+package com.pengrad.keezy.sound;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+
+/**
+ * User: stas
+ * Date: 07.05.14 23:19
+ */
+
+public class MediaPlayerManager implements PlayManager {
+
+    private MediaPlayer[] mediaPlayers;
+    private Context context;
+
+    public MediaPlayerManager(Context context, int size) {
+        this.context = context;
+        mediaPlayers = new MediaPlayer[size];
+    }
+
+    public void addSound(int index, String path) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Uri.parse(path));
+        mediaPlayers[index] = mediaPlayer;
+    }
+
+    public void removeSound(int index) {
+        MediaPlayer old = mediaPlayers[index];
+        if (old != null) {
+            old.release();
+            mediaPlayers[index] = null;
+        }
+    }
+
+    public void startPlay(int index) {
+        MediaPlayer mediaPlayer = mediaPlayers[index];
+        if (mediaPlayer == null) return;
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            mediaPlayer.seekTo(0);
+        }
+        mediaPlayer.start();
+    }
+}
