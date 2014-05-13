1e486fb5bbd3e3a3fa41abab2fac67f3d689cd52
Stas Parshin <pengrad@bk.ru>
2014-05-13 22:51:01 +0400

    Refactoring. Bring out WAV-file creating to separate class

--- Keezy/src/main/java/com/pengrad/keezy/sound/AudioRecordManager.java
+++ Keezy/src/main/java/com/pengrad/keezy/sound/AudioRecordManager.java
@@ -5,9 +5,6 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Process;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,8 +36,9 @@ public class AudioRecordManager implements RecordManager {
     private static class RecordAudio implements Runnable {
         public static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
         public static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+        public static final byte CHANNEL_COUNT = 1;
+        public static final byte BITS_PER_SAMPLE = 16;
         public static final int SAMPLE_RATE = 44100;   // 22050, 11025, 16000, 8000  44100
-        public static final int BITS_PER_SAMPLE = 16;
         public static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
 
         private volatile boolean cancel = false;
@@ -57,7 +55,7 @@ public class AudioRecordManager implements RecordManager {
 
             AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
             List<byte[]> recordList = new ArrayList<byte[]>();
-            int readSum = 0;
+//            int readSum = 0;
 
             audioRecord.startRecording();
             while (!cancel) {
@@ -70,87 +68,15 @@ public class AudioRecordManager implements RecordManager {
                     bb.get(data, 0, data.length);
                     recordList.add(data);
                 }
-                readSum += bufferReadResult;
+//                readSum += bufferReadResult;
             }
             audioRecord.stop();
             audioRecord.release();
 
-            writeWaveFile(file, recordList, readSum);
+            WavFileCreator.writeWavFile(file, recordList, SAMPLE_RATE, BITS_PER_SAMPLE, CHANNEL_COUNT);
 
             if (endCallback != null) endCallback.run();
         }
-
-        private void writeWaveFile(String outFilename, List<byte[]> data, int size) {
-            long longSampleRate = SAMPLE_RATE;
-            int channels = 1;
-            long byteRate = BITS_PER_SAMPLE * SAMPLE_RATE * channels / 8;
-            try {
-                FileOutputStream out = new FileOutputStream(outFilename);
-                int totalAudioLen = size;
-                int totalDataLen = totalAudioLen + 36;
-
-                writeWaveFileHeader(out, totalAudioLen, totalDataLen, longSampleRate, channels, byteRate);
-
-                for (byte[] bytes : data) out.write(bytes);
-
-                out.flush();
-                out.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private void writeWaveFileHeader(FileOutputStream out, long totalAudioLen, long totalDataLen, long longSampleRate, int channels, long byteRate) throws IOException {
-            byte[] header = new byte[44];
-
-            header[0] = 'R';  // RIFF/WAVE header
-            header[1] = 'I';
-            header[2] = 'F';
-            header[3] = 'F';
-            header[4] = (byte) (totalDataLen & 0xff);
-            header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-            header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-            header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-            header[8] = 'W';
-            header[9] = 'A';
-            header[10] = 'V';
-            header[11] = 'E';
-            header[12] = 'f';  // 'fmt ' chunk
-            header[13] = 'm';
-            header[14] = 't';
-            header[15] = ' ';
-            header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
-            header[17] = 0;
-            header[18] = 0;
-            header[19] = 0;
-            header[20] = 1;  // format = 1
-            header[21] = 0;
-            header[22] = (byte) channels;
-            header[23] = 0;
-            header[24] = (byte) (longSampleRate & 0xff);
-            header[25] = (byte) ((longSampleRate >> 8) & 0xff);
-            header[26] = (byte) ((longSampleRate >> 16) & 0xff);
-            header[27] = (byte) ((longSampleRate >> 24) & 0xff);
-            header[28] = (byte) (byteRate & 0xff);
-            header[29] = (byte) ((byteRate >> 8) & 0xff);
-            header[30] = (byte) ((byteRate >> 16) & 0xff);
-            header[31] = (byte) ((byteRate >> 24) & 0xff);
-            header[32] = (byte) (2 * 16 / 8);  // block align
-            header[33] = 0;
-            header[34] = BITS_PER_SAMPLE;  // bits per sample
-            header[35] = 0;
-            header[36] = 'd';
-            header[37] = 'a';
-            header[38] = 't';
-            header[39] = 'a';
-            header[40] = (byte) (totalAudioLen & 0xff);
-            header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-            header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-            header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
-
-            out.write(header, 0, 44);
-        }
     }
+
 }
---
+++ Keezy/src/main/java/com/pengrad/keezy/sound/WavFileCreator.java
@@ -0,0 +1,88 @@
+package com.pengrad.keezy.sound;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * User: stas
+ * Date: 13.05.14 22:10
+ */
+
+public class WavFileCreator {
+
+    public static void writeWavFile(String outFilename, List<byte[]> data, int sampleRate, byte bitsPerSample, byte channelCount) {
+        try {
+            int totalAudioLen = 0;
+            for (byte[] bytes : data) totalAudioLen += bytes.length;
+            int totalDataLen = totalAudioLen + 36;
+            int byteRate = bitsPerSample * sampleRate * channelCount / 8;
+
+            FileOutputStream out = new FileOutputStream(outFilename);
+
+            writeWaveFileHeader(out, totalAudioLen, totalDataLen, sampleRate, channelCount, byteRate, bitsPerSample);
+
+            for (byte[] bytes : data) out.write(bytes);
+
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeWaveFileHeader(FileOutputStream out, int totalAudioLen, int totalDataLen, int sampleRate, int channelCount, int byteRate, byte bitsPerSample) throws IOException {
+        byte[] header = new byte[44];
+
+        header[0] = 'R';  // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f';  // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1;  // format = 1
+        header[21] = 0;
+        header[22] = (byte) channelCount;
+        header[23] = 0;
+        header[24] = (byte) (sampleRate & 0xff);
+        header[25] = (byte) ((sampleRate >> 8) & 0xff);
+        header[26] = (byte) ((sampleRate >> 16) & 0xff);
+        header[27] = (byte) ((sampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8);  // block align
+        header[33] = 0;
+        header[34] = bitsPerSample;  // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+        out.write(header, 0, 44);
+    }
+
+}
